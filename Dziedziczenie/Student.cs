5db25a98aeb8fe58fb50dae6fda97298f2dbaabe
sebnorth <sebnorth@gmail.com>
2020-04-07 16:14:21 +0200

    Uzupełniliśmy klasę student i dodaliśmy testy.

--- Dziedziczenie/Student.cs
+++ Dziedziczenie/Student.cs
@@ -8,5 +8,22 @@ namespace Dziedziczenie
 {
     class Student : Osoba
     {
+        private int rok;
+        private int grupa;
+        private int nrIndeksu;
+
+        public Student(string imie_, string nazwisko_, string dataurodzenia_, int rok_, int grupa_, int nrIndeksu_) : base(imie_, nazwisko_, dataurodzenia_)
+
+        {
+            this.rok = rok_;
+            this.grupa = grupa_;
+            this.nrIndeksu = nrIndeksu_;
+        }
+
+        public override void WypiszInfo()
+        {
+            base.WypiszInfo();
+            Console.WriteLine($"Rok studiów: {this.rok}, grupa: {this.grupa}, numer indeksu: {this.nrIndeksu}");
+        }
     }
 }
